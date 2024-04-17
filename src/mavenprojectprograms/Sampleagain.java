08d6f2d59f9c3f99cf0f5158044b109163b273af
sairarache <136586853+sairarache@users.noreply.github.com>
2024-04-17 12:44:26 +0100

    latest

---
+++ src/mavenprojectprograms/Sampleagain.java
@@ -0,0 +1,10 @@
+package mavenprojectprograms;
+
+public class Sampleagain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hai,hello");
+	}
+
+}
